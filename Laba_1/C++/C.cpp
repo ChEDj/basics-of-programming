675493bcff50b2deaf5b9c6f6ff96cfcd1189155
Edem <edemcherkezov@gmail.com>
2021-09-14 10:45:28 +0300

    add all

--- Laba_1/C++/C.cpp
+++ Laba_1/C++/C.cpp
@@ -1,6 +1,5 @@
 ﻿
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
@@ -9,9 +8,9 @@ int main()
 	float side; //Змінна для сторони квадрату
 	cout << "Enter side of square:" << endl;
 	cin >> side; //Введення сторони квадрату
-	float diagonal = sqrt(2 * pow(side, 2));//Обчислення діагоналі
-	float perimeter = 4 * side;//Обчислення периметру
-	float area = pow(side, 2);//Обчислення площи
+	float diagonal = sqrt(2 * pow(side, 2)); //Обчислення діагоналі
+	float perimeter = 4 * side; //Обчислення периметру
+	float area = pow(side, 2); //Обчислення площи
 	cout << "Diagonal = " << diagonal << endl; //Виведення результату
 	cout << "Perimeter = " << perimeter << endl; //Виведення результату
 	cout << "Area = " << area << endl; //Виведення результату
